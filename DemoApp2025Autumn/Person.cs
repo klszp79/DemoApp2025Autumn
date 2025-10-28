0be06512a1dc27460f72c5b40479b5e9da4b8d58
llaszlo <laszlopeter.kiss@emerson.com>
2025-10-28 08:42:02 +0100

    Added validation

--- DemoApp2025Autumn/Person.cs
+++ DemoApp2025Autumn/Person.cs
@@ -1,12 +1,19 @@
-﻿namespace DemoApp2025Autumn.App
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoApp2025Autumn.App
 {
     public class Person
     {
+        [Required]
         public string Id { get; set; }
 
+        [Required]
+        [MaxLength(20)]
         public string Name { get; set; } = string.Empty;
+        [EmailAddress]
         public string EmailAddress { get; set; }
-
+        
+        [Range(typeof(DateOnly), "1990-01-01","2000-12-31")]
         public DateOnly Birthday { get; set; }
     }
 }
